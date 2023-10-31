130a2f0d686385c798b21b3104acb7d6496c4a25
davilafernando123456789 <jose.davila.b@tecsup.edu.pe>
2023-10-30 23:55:42 -0500

    subiendo validacion de nombre

--- controllers/userController.js
+++ controllers/userController.js
@@ -36,64 +36,66 @@ const config = require('../config/global');
 // }
 
 exports.crearUsuario = async (req, res) => {
-   const { username, email, password } = req.body;
-    
-        if (!username || !email || !password) {
-          return res.status(400).json({ auth: false, token: null, error: "campos obligatorios" });
-        }
-  
-    try {
-      const existingUser = await User.findOne({ email: email });
-      if (existingUser) {
-        return res.status(409).json({ auth: false, token: null, error: "usuario existente" });
-      }
-  
-      const user = new User({
+  const { username, email, password } = req.body;
+   
+  if (!username || !email || !password) {
+     return res.status(400).json({ auth: false, token: null, error: "Campos obligatorios" });
+  }
+
+  try {
+     const existingUser = await User.findOne({ email: email });
+     if (existingUser) {
+        return res.status(409).json({ auth: false, token: null, error: "Usuario existente" });
+     }
+
+     const user = new User({
         username,
         email,
         password
-      });
-  
-      user.password = await user.encryptPassword(user.password);
-      await user.save();
-  
-      const token = jwt.sign({ id: user._id }, config.secret, {
+     });
+
+     user.password = await user.encryptPassword(user.password);
+     await user.save();
+
+     const token = jwt.sign({ id: user._id }, config.secret, {
         expiresIn: 60 * 60 * 24
-      });
+     });
+
+     res.json({ auth: true, token, username: user.username }); // Incluimos el nombre de usuario en la respuesta.
+  } catch (error) {
+     console.log(error);
+     res.status(500).send('Hubo un error');
+  }
+};
+
   
-      res.json({ auth: true, token });
-    } catch (error) {
-      console.log(error);
-      res.status(500).send('Hubo un error');
+exports.obtenerUsuario = async (req, res) => {
+  const { email, password } = req.body;
+
+  try {
+    const user = await User.findOne({ email: email });
+
+    if (!user) {
+      return res.status(401).json({ auth: false, token: null, error: "Usuario no encontrado" });
     }
-  };
-  
-  exports.obtenerUsuario = async (req, res) => {
-    const { email, password } = req.body;
-  
-    try {
-      const user = await User.findOne({ email: email });
-  
-      if (!user) {
-        return res.status(401).json({ auth: false, token: null, error: "usuario no encontrado" });
-      }
-  
-      const validPassword = await user.validatePassword(password);
-  
-      if (!validPassword) {
-        return res.status(401).json({ auth: false, token: null, error: "contraseña incorrecta" });
-      }
-  
-      const token = jwt.sign({ id: user._id }, config.secret, {
-        expiresIn: 60 * 60 * 24
-      });
-  
-      res.json({ auth: true, token });
-    } catch (error) {
-      console.log(error);
-      res.status(500).send('Hubo un error');
+
+    const validPassword = await user.validatePassword(password);
+
+    if (!validPassword) {
+      return res.status(401).json({ auth: false, token: null, error: "Contraseña incorrecta" });
     }
-  };
+
+    const token = jwt.sign({ id: user._id }, config.secret, {
+      expiresIn: 60 * 60 * 24
+    });
+
+    res.json({ auth: true, token, username: user.username }); // Incluimos el nombre de usuario en la respuesta.
+  } catch (error) {
+    console.log(error);
+    res.status(500).send('Hubo un error');
+  }
+};
+
   
 
 // exports.obtenerUsuario = async (req, res) => {
